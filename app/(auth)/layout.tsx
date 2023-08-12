0f261ffeb46d124ccb629262caecc0a7a3323b08
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-08-12 11:33:44 -0500

    Setup layouts and auth with Clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import '../globals.css';
+
+export const metadata: Metadata = {
+    title: 'Threads',
+    description: 'A Meta Threads Clone built with Next.js 13'
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+    children
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <ClerkProvider
+      >
+        <html lang='en'>
+          <body className={`${inter.className} bg-dark-1`}>
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
+    );
+  }
---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,7 @@
+async function Page() {
+    return (
+        <main>
+            <h1>Onboarding</h1>
+        </main>
+    )
+}
