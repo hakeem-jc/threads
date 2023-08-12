cab935e0f8de07b8b763d9b71d1179c658cb848a
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-08-12 12:44:14 -0500

    Create page layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,28 +1,31 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '../globals.css';
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-export const metadata: Metadata = {
-    title: 'Threads',
-    description: 'A Meta Threads Clone built with Next.js 13'
-}
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: 'A Meta Threads Clone built with Next.js 13'
+};
+
 export default function RootLayout({
-    children
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <ClerkProvider
-      >
-        <html lang='en'>
-          <body className={`${inter.className} bg-dark-1`}>
-            {children}
-          </body>
-        </html>
-      </ClerkProvider>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
